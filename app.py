4a6e4067c828d0c8705c8af5f1aca9daaf6d9347
Ali <aliulmg4@gmail.com>
2021-10-08 19:44:56 +0330

    upgrade sql

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ PASSWORD = os.environ.get("PASSWORD")
 login_manager = LoginManager()
 app = Flask(__name__)
 login_manager.init_app(app)
-app.secret_key = "what the hell?"
+app.secret_key = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 ckeditor = CKEditor(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog-posts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 gravatar = Gravatar(app, size=70, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 db = SQLAlchemy(app=app)
